6a683ca500a32a34a9f28ad1b6d3b75f823ee822
STEAMAN1 <91830548+STEAMAN1@users.noreply.github.com>
2024-11-28 17:14:41 +0900

    Create sum.c

---
+++ sum.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(void)
+{
+    printf("Hello World!!");
+    int num == 10;
+
+    int result = 0;
+    for (int i = 0; i < num; i++) {
+        result += i;
+
+    printf("result : %d", result);
+}
