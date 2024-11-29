35f95d4af116816b5535f98a126ed653e4aef685
STEAMAN1 <gohgoh0512@gmail.com>
2024-11-29 10:37:31 +0900

    Startcommit

--- sum.c
+++ sum.c
@@ -2,12 +2,15 @@
 
 int main(void)
 {
-    printf("Hello World!!");
-    int num == 10;
+    printf("Hello World!!\n");
+    int num;
+    printf("ENTER : ");
+    scanf_s("%d", num);
 
     int result = 0;
     for (int i = 0; i < num; i++) {
         result += i;
+    }
 
     printf("result : %d", result);
 }
